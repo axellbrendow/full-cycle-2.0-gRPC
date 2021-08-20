a07bca448704c13d339f610ae09330146028da3b
axell-brendow <breno.axel@gmail.com>
2021-08-20 08:05:43 -0300

    Add back-end services

---
+++ services/user.go
@@ -0,0 +1,121 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"github.com/axell-brendow/full-cycle-2.0-gRPC/pb"
+)
+
+// type UserServiceServer interface {
+// 	AddUser(context.Context, *User) (*User, error)
+// 	mustEmbedUnimplementedUserServiceServer()
+// }
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+	// insert db
+
+	fmt.Println(req.Name)
+
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
+	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the stream message: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
+}
+
+func (*UserService) AddUserBidirectionalStream(stream pb.UserService_AddUserBidirectionalStreamServer) error {
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the stream message: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   req,
+		})
+
+		if err != nil {
+			log.Fatalf("Coult not send data to the stream: %v", err)
+		}
+	}
+}
